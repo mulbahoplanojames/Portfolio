0fafb9b98e2cbbaad61228386ba555139bb131fc
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-12-13 00:55:19 +0200

    Completed the Navbar and Footer

---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+import { JSX } from "react";
+
+export type NavLinksType = {
+  label: string;
+  path: string;
+};
+
+export type SocialsType = {
+  id: number;
+  icon: JSX.Element;
+  link: string;
+};
